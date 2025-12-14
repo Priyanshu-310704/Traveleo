9d2a8d429978b3c8521f80eb7dd78878a9a8a0d0
Priyanshu <prysh310704@gmail.com>
2025-12-14 17:41:16 +0530

    cors fixed backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,19 @@
 import express from 'express'
+import cors from "cors";
+
 const app=express()
 
 //middleware
 app.use(express.json())
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // frontend URL
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true
+  })
+);
 
 import userRoute from "./routes/user.route.js";//users
 
